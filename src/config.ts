a9a0e812f2e15cd39709b1222e817e7910054f1b
Git Sync <git-sync@localhost>
2026-05-27 11:07:40 -0700

    feat(project): project intialization

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,19 @@
-import convict from "convict";
+import convict from 'convict';
 
 export interface ServerConfig {
-	port: number;
+    port: number;
 }
 
 const configSchema = convict<ServerConfig>({
-	port: {
-		doc: "The port to bind.",
-		format: "port",
-		default: 3000,
-		env: "PORT",
-		arg: "port",
-	},
+    port: {
+        doc: 'The port to bind.',
+        format: 'port',
+        default: 3000,
+        env: 'PORT',
+        arg: 'port',
+    },
 });
 
-configSchema.validate({ allowed: "strict" });
+configSchema.validate({ allowed: 'strict' });
 
 export const config = configSchema.getProperties();
