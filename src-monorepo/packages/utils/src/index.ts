92ad3714e1e42c547d42f16d66c391caf035d36b
Git Sync <git-sync@localhost>
2026-05-27 22:12:12 -0700

    feat(project): project intialization

--- src-monorepo/packages/utils/src/index.ts
+++ src-monorepo/packages/utils/src/index.ts
@@ -7,10 +7,6 @@ export type Prettify<T> = {
     [K in keyof T]: T[K];
 } & {};
 
-export function add(a: number, b: number): number {
-    return a + b;
-}
-
 export type { ZodSchema, ZodType, ZodTypeDef, z as ZodNamespace } from 'zod';
 // Re-export zod so every workspace package gets it through utils.
 export { z } from 'zod';
