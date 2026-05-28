1e97dfc149ce3d7e7048a5b7ba6d23f114ed7ba7
Git Sync <git-sync@localhost>
2026-05-28 10:07:42 -0700

    feat(project): project intialization

--- src-monorepo/apps/web/src/orpc.ts
+++ src-monorepo/apps/web/src/orpc.ts
@@ -3,7 +3,7 @@ import { createORPCClient } from '@orpc/client';
 import { RPCLink } from '@orpc/client/fetch';
 
 const link = new RPCLink({
-    url: 'http://localhost:3000/rpc',
+    url: import.meta.env.VITE_API_URL ?? 'http://localhost:3000/rpc',
 });
 
 export const orpc: ContractRouterClient<typeof planetContract> = createORPCClient(link);
