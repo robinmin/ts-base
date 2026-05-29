9c25c939b19f320eab8a096ac83a0446cdc5f783
Git Sync <git-sync@localhost>
2026-05-28 23:19:26 -0700

    feat(project): project intialization

--- src-cli/packages/utils/tests/index.test.ts
+++ src-cli/packages/utils/tests/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'bun:test';
-import { add } from '../src/index.js';
+import { add } from '../src/index';
 
 describe('add', () => {
     it('sums two numbers', () => {
